b7daccec5fa94040140421eb3cbbc35b37a2c268
Sen Hong <senhong0118@gmail.com>
2019-04-06 12:17:11 +1000

    db

--- includes/db.php
+++ includes/db.php
@@ -7,10 +7,12 @@ $db['db_name'] = "cms";
 
 
 
+foreach ($db as $key => $value) {
+    define(strtoupper($key),$value);
+}
 
 
-
-$connection = mysqli_connect('localhost','root','','cms');
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
 if($connection){
   echo "we are connected";
