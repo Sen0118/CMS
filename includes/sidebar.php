28cfe04de743a2f948ad5e11db4f4214c2876dad
Sen Hong <senhong0118@gmail.com>
2019-04-07 08:31:27 +1000

    login

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -14,6 +14,28 @@
     <!-- /.input-group -->
 </div>
 
+
+<!-- login -->
+<div class="well">
+    <h4>Login</h4>
+    <form action="includes/login.php" method="post">
+    <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Enter Username">
+
+    </div>
+
+    <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+        <span class="input-group-btn">
+        <button name="login" class="btn btn-primary" name="login" type="submit">
+        Submit
+        </button>
+        </span>
+    </div>
+    </form>
+    <!-- /.input-group -->
+</div>
+
 <!-- Blog Categories Well -->
 <div class="well">
     <h4>Blog Categories</h4>
